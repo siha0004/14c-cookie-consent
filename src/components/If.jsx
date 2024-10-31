d9863c19181619d668fde5824d295e3713f4512b
Sif Melkane <siha0004@stud.kea.dk>
2024-10-31 11:30:29 +0100

    Leftovers

--- src/components/If.jsx
+++ src/components/If.jsx
@@ -1,18 +1,18 @@
-// LØSNING MED CONDITIONAL RENDERING
+// 1. LØSNING MED CONDITIONAL RENDERING
 const If = ({ children, condition }) => {
   return <>{condition && children}</>;
 };
 
 export default If;
 
-// LØSNING MED TERNARY OPERATOR
+// 2. LØSNING MED TERNARY OPERATOR
 // const If = ({ children, condition }) => {
 //   return <>{condition ? children : "Her er tomt"}</>;
 // };
 
 // export default If;
 
-// LØSNING MED IF/ELSE
+// 3. LØSNING MED IF/ELSE
 // const If = ({ children, condition }) => {
 //   let content;
 //   if (condition) {
