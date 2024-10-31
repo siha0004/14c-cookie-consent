7570bbdf65a2006920c548114b7c71017dba9901
Sif Melkane <siha0004@stud.kea.dk>
2024-10-31 11:37:17 +0100

    Changing names

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,10 @@ import If from "@/components/If";
 import Image from "next/image";
 
 export default function Home() {
-  const isPopupTrue = true;
+  const showPopup = true;
   return (
     <div>
-      <If condition={isPopupTrue}>
+      <If condition={showPopup}>
         <CookieConsent></CookieConsent>
       </If>
     </div>
