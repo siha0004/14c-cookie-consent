be66c7d6aa743edc74ed30fd624a5c5935700c36
Sif Melkane <siha0004@stud.kea.dk>
2024-10-31 11:30:12 +0100

    third solution

--- src/components/If.jsx
+++ src/components/If.jsx
@@ -1,13 +1,26 @@
 // LØSNING MED CONDITIONAL RENDERING
+const If = ({ children, condition }) => {
+  return <>{condition && children}</>;
+};
+
+export default If;
+
+// LØSNING MED TERNARY OPERATOR
 // const If = ({ children, condition }) => {
-//   return <>{condition && children}</>;
+//   return <>{condition ? children : "Her er tomt"}</>;
 // };
 
 // export default If;
 
-// LØSNING MED TERNARY OPERATOR
-const If = ({ children, condition }) => {
-  return <>{condition ? children : "Her er tomt"}</>;
-};
+// LØSNING MED IF/ELSE
+// const If = ({ children, condition }) => {
+//   let content;
+//   if (condition) {
+//     content = children;
+//   } else {
+//     content = "Her er tomt";
+//   }
+//   return content;
+// };
 
-export default If;
+// export default If;
