0063a2809af1a4d3e691563db25138df099998de
Sif Melkane <siha0004@stud.kea.dk>
2024-10-31 11:46:43 +0100

    Dark mode added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,5 @@
 const Button = ({ text }) => {
-  return <button className="bg-white rounded-lg px-12 py-2 mt-5">{text}</button>;
+  return <button className="bg-white dark:bg-black rounded-lg px-12 py-2 mt-5">{text}</button>;
 };
 
 export default Button;
--- src/components/CookieConsent.jsx
+++ src/components/CookieConsent.jsx
@@ -4,12 +4,12 @@ import { MdClose } from "react-icons/md";
 
 const CookieConsent = () => {
   return (
-    <article className="flex flex-col w-64 bg-black px-7 py-6 rounded-2xl m-5">
+    <article className="flex flex-col w-64 bg-black dark:bg-white px-7 py-6 rounded-2xl m-5">
       <div className="flex justify-between	">
-        <LiaCookieBiteSolid className="text-white size-16" />
-        <MdClose className="text-white" />
+        <LiaCookieBiteSolid className="text-white dark:text-black size-16" />
+        <MdClose className="text-white dark:text-black" />
       </div>
-      <p className=" text-white text-xl font-medium mt-14">We use cookies to improve your user experience.</p>
+      <p className=" text-white dark:text-black text-xl font-medium mt-14">We use cookies to improve your user experience.</p>
       <Button text="I love Cookies"></Button>
     </article>
   );
