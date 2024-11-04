9f7cd3951829e155f39dfa5cdd808886abe5d6ad
Sif Melkane <siha0004@stud.kea.dk>
2024-11-04 12:21:52 +0100

    Adding states (button nolonger its own component

--- src/components/CookieConsent.jsx
+++ src/components/CookieConsent.jsx
@@ -1,17 +1,29 @@
+"use client";
+
 import Button from "./Button";
 import { LiaCookieBiteSolid } from "react-icons/lia";
 import { MdClose } from "react-icons/md";
+import { useState } from "react";
 
 const CookieConsent = () => {
+  const [isCookieClosed, setisCookieClosed] = useState(false);
+  const [isCookieAccepted, setisCookieAccepted] = useState(false);
   return (
-    <article className="flex flex-col w-64 bg-black dark:bg-white px-7 py-6 rounded-2xl m-5">
-      <div className="flex justify-between	">
-        <LiaCookieBiteSolid className="text-white dark:text-black size-16" />
-        <MdClose className="text-white dark:text-black" />
-      </div>
-      <p className=" text-white dark:text-black text-xl font-medium mt-14">We use cookies to improve your user experience.</p>
-      <Button text="I love Cookies"></Button>
-    </article>
+    !isCookieClosed &&
+    !isCookieAccepted && (
+      <article className="flex flex-col max-w-64 bg-black dark:bg-white px-7 py-6 rounded-2xl m-5">
+        <header className="flex justify-between	items-start">
+          <LiaCookieBiteSolid className="text-white dark:text-black size-16" />
+          <button onClick={() => setisCookieClosed(!isCookieClosed)}>
+            <MdClose className="text-white dark:text-black" />
+          </button>
+        </header>
+        <p className=" text-white dark:text-black text-xl font-medium mt-14">We use cookies to improve your user experience.</p>
+        <button onClick={() => setisCookieAccepted(!isCookieAccepted)} className="bg-white dark:bg-black rounded-lg px-12 py-2 mt-5">
+          I love Cookies
+        </button>
+      </article>
+    )
   );
 };
 
